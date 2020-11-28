dc2bc63c2ed06957a46f2dfc8cb58b5b73f8c38e
HyoTaekJang <hyotaek9812@gmail.com>
2020-11-29 01:55:18 +0900

    borderlayout 성공, 색변경해야함

--- 객프실습13주/src/객프실습13주/ActionFrame.java
+++ 객프실습13주/src/객프실습13주/ActionFrame.java
@@ -2,16 +2,21 @@ package ╟╢га╫г╫ю13аж;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 import javax.swing.*;
 
 // A frame with a panel that demonstrates color change actions.
 public class ActionFrame extends JFrame {
 	private JPanel buttonPanel;
-	private static final int DEFAULT_WIDTH = 300;
-	private static final int DEFAULT_HEIGHT = 200;
+	JPanel DrawPanel = new JPanel();
+//	private static final int DEFAULT_WIDTH = 1000;
+//	private static final int DEFAULT_HEIGHT = 1000;
 
 	public ActionFrame() {
-		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		//setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		buttonPanel = new JPanel();
 // define actions
 		Action yellowAction = new ColorAction("Yellow", new ImageIcon("yellow-ball.gif"), Color.YELLOW);
@@ -22,7 +27,12 @@ public class ActionFrame extends JFrame {
 		buttonPanel.add(new JButton(blueAction));
 		buttonPanel.add(new JButton(redAction));
 //add panel to frame
-		add(buttonPanel);
+		setLayout(new BorderLayout());
+		add(buttonPanel, BorderLayout.NORTH);
+		add(new DrawComponent(), BorderLayout.CENTER);
+		DrawPanel.add(new DrawComponent());
+		add(DrawPanel, BorderLayout.CENTER);
+		pack();
 //associate the Y, B, and R keys with names
 		InputMap imap = buttonPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		imap.put(KeyStroke.getKeyStroke("ctrl Y"), "panel.yellow");
@@ -44,8 +54,48 @@ public class ActionFrame extends JFrame {
 		}
 
 		public void actionPerformed(ActionEvent event) {
-			Color c = (Color) getValue("color");
-			buttonPanel.setBackground(c);
+			
+			DrawPanel.add(new DrawComponent());
+			add(DrawPanel, BorderLayout.CENTER);
+			
+//			Color c = (Color) getValue("color");
+//			buttonPanel.setBackground(c);
 		}
 	} // end of inner class ColorAction
+
+	class DrawComponent extends JComponent {
+		private static final int DEFAULT_WIDTH = 400;
+		private static final int DEFAULT_HEIGHT = 400;
+		
+		public void paintComponent(Graphics g) {
+			Graphics2D g2 = (Graphics2D) g;
+			
+			g2.setColor(Color.BLUE);
+			
+			double leftX = 100;
+			double topY = 100;
+			double width = 200;
+			double height = 150;
+			Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
+			g2.draw(rect);
+//draw the enclosed ellipse
+			Ellipse2D ellipse = new Ellipse2D.Double();
+			ellipse.setFrame(rect);
+			g2.draw(ellipse);
+////draw a diagonal line
+			g2.draw(new Line2D.Double(leftX, topY, leftX + width, topY + height));
+//draw a circle with the same center
+			double centerX = rect.getCenterX();
+			double centerY = rect.getCenterY();
+			double radius = 150;
+			Ellipse2D circle = new Ellipse2D.Double();
+			circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+			g2.draw(circle);
+		}
+		
+		public Dimension getPreferredSize() {
+			return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		}
+	}
+
 }
